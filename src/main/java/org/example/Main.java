0f52bd709d1000682041c4dc6689873148db72fe
shreyansh <skhand17@asu.edu>
2024-04-09 12:37:47 -0600

    Adding the Main.java

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("This is my first commit");
+    System.out.println("Learning Git from Telusko");
+  }
+}
