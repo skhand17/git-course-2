073b0634dc53fd2e02805f70d31754d02045af07
shreyansh <skhand17@asu.edu>
2024-04-09 19:07:13 -0600

    Added Employer Class

---
+++ src/main/java/org/example/Employer.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Employer {
+
+  private String empName;
+
+  public Employer(String empName) {
+    this.empName = empName;
+  }
+
+  public String getEmpName() {
+    return empName;
+  }
+
+  public void setEmpName(String empName) {
+    this.empName = empName;
+  }
+}
